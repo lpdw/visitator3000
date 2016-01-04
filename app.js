c0f695d9c4a7e21d72dd4648d5e389c598828560
Yassin AGHANI <aghani.yassin@gmail.com>
2016-01-04 19:32:16 +0100

    Add express.js web framework

--- app.js
+++ app.js
@@ -1,19 +1,33 @@
-var five = require("johnny-five"),
-button;
+var five = require("johnny-five");
 var mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/test');
+var express = require('express');
+var expressLayouts = require('express-ejs-layouts');
+var app = express();
 
+mongoose.connect('mongodb://localhost/test');
 var Visit = mongoose.model('Visit', { at: { type: Date, default: Date.now } });
+
+app.set('view engine', 'ejs');
+app.set('layout', 'layout');
+
+app.use(expressLayouts);
+app.use(express.static('public'));
+
+app.get('/', function(req, res) {
+    res.render('home');
+});
+
+app.listen(process.env.VISITATOR_3000_PORT || 8080);
 
 five.Board().on("ready", function() {
 
-  button = new five.Button({
+  var button = new five.Button({
     pin: 2,
     isPullup: true
   });
 
   button.on("down", function(value) {
-    console.log('Nouvelle visite');
+    console.log('New visit');
     var visit = new Visit();
     visit.save(function (err) {
       if (err){
