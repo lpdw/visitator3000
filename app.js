b7b89c53bfabae262e31e603ff7bcdfdaf9ca757
Yassin AGHANI <aghani.yassin@gmail.com>
2016-01-05 14:24:08 +0100

    Display REAL visits counter with LCD screen

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var mongoose = require('mongoose');
 var express = require('express');
 var expressLayouts = require('express-ejs-layouts');
 var app = express();
+var lcd;
 
 mongoose.connect('mongodb://localhost/test');
 var Visit = mongoose.model('Visit', { at: { type: Date, default: Date.now } });
@@ -15,12 +16,20 @@ app.use(expressLayouts);
 app.use(express.static('public'));
 
 app.get('/', function(req, res) {
-    res.render('home');
+  res.render('home');
 });
 
 app.listen(process.env.VISITATOR_3000_PORT || 8080);
 
 five.Board().on("ready", function() {
+  lcd = new five.LCD({
+    pins: [7, 8, 9, 10, 11, 12],
+    backlight: 6,
+    rows: 2,
+    cols: 4
+  });
+
+  refreshLcd(lcd);
 
   var button = new five.Button({
     pin: 2,
@@ -33,20 +42,26 @@ five.Board().on("ready", function() {
     visit.save(function (err) {
       if (err){
         console.log("ERROR: ", err);
+      } else {
+        refreshLcd(lcd);
       }
     });
   });
 
-  lcd = new five.LCD({
-      pins: [7, 8, 9, 10, 11, 12],
-      backlight: 6,
-      rows: 2,
-      cols: 4
-    });
+});
 
+function refreshLcd(lcd){
   lcd.cursor(0, 0).print("JOUR MOIS  ANNEE");
 
-  lcd.cursor(1, 0).print("5");
-  lcd.cursor(1, 5).print("12");
-  lcd.cursor(1, 11).print("2005");
-});
+  Visit.$where('return this.at.getDay() == (new Date).getDay()').exec(function (err, docs) {
+    lcd.cursor(1, 0).print(docs.length);
+  });
+
+  Visit.$where('return this.at.getMonth() == (new Date).getMonth()').exec(function (err, docs) {
+    lcd.cursor(1, 5).print(docs.length);
+  });
+
+  Visit.$where('return this.at.getYear() == (new Date).getYear()').exec(function (err, docs) {
+    lcd.cursor(1, 11).print(docs.length);
+  });
+}
