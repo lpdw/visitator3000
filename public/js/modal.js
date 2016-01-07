be17e97c8bc5e3d75b0a5736408693398a5bcb39
Mangubu <jdmangubu@gmail.com>
2016-01-07 13:13:35 +0100

    fixed datepicker

--- public/js/modal.js
+++ public/js/modal.js
@@ -1,7 +1,12 @@
 $( document ).ready(function() {
   $( ".modal-trigger" ).click(function() {
     $('#modal1').openModal();
+    $('.datepicker').pickadate({
+      // Strings and translations
+      monthsFull: ['Janvier', 'Fevrier', 'Mars', 'Avril', 'Mai', 'Juin', 'Juillet', 'Aôut', 'Septembre', 'Octobre', 'Novembre', 'Decembre'],
+      monthsShort: ['Jan', 'Fev', 'Mar', 'Avr', 'Mai', 'Jui', 'Juil', 'Aou', 'Sep', 'Oct', 'Nov', 'Dec'],
+      weekdaysFull: ['Dimanche', 'Lundi', 'Mardi', 'Mercredi', 'Jeudi', 'Vendredi', 'Samedi'],
+      weekdaysShort: ['Dim', 'Lun', 'Mar', 'Mer', 'Jeu', 'Ven', 'Sam'],
+    });
   });
-
-  $('#datePickerVisits').datepicker();
 });
